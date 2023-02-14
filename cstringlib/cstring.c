16067fa040f0b47d876ff89aa6c66a07b1d25da4
wuyuxin <102937978+woshishabi1@users.noreply.github.com>
2023-02-14 18:15:13 +0800

    v0.6 Added four functions
    pushlists，poplists，swapstr and repstr

--- cstringlib/cstring.c
+++ cstringlib/cstring.c
@@ -20,7 +20,7 @@ if(Astr.length == Bstr.length){
 }else{return -1;}
 	return 0;
 }
-void begin(cstring* start)
+void begin(cstring* start)//cao,原来c语言结构体是不能初始化的 
 {
 		start->data=NULL;//free空指针是安全的 
 		start->olddata=NULL;
@@ -42,7 +42,7 @@ bool setdata_lim(cstring *tar,char* st,unsigned start,unsigned int end){
 				//printf("\nmem: %d\n",(end+1-start)*sizeof(char));//debug
 					if(tar->data==NULL){return false;}
 					tar->length=end+1-start;
-					for(int i=start;i<end;i++){
+					for(unsigned i=start;i<end;i++){
 				     *(tar->data+i-start)=*(st+i);	
 				     //printf("%c",*(tar->data+i-start));
 				     
@@ -53,7 +53,7 @@ bool setdata_lim(cstring *tar,char* st,unsigned start,unsigned int end){
 					return true;
 		}
 bool setdata(cstring *tar,char* st){
-		if(setdata_lim(tar,st,0,len(st)-1)==true){return true;}else{return false;}
+		if(setdata_lim(tar,st,0,len(st)-1)==true){return true;}else{return false;}//害人不浅
 	}
 	
 bool cpstr(cstring *tostr,cstring *tarstr){
@@ -78,7 +78,48 @@ void add_str(cstring *todata,cstring *str){
 		*(todata->data+todata->length+str->length-2)='\0';
 		//printf("%d",length-1+str.length+500);//debug 
 		todata->length=todata->length+str->length-1;
-		free(todata->olddata);
+		free(todata->olddata);//free是个十分甚至九分好的习惯:) 
 		
 		
 	}
+void pushlist(cstring *lis,char val) {
+    lis->olddata = lis->data;//backup
+    lis->data = (char*)malloc(sizeof(char)*(lis->length+1));
+    
+
+    for (int i = 0; i < lis->length-1; i++) {
+        *(lis->data + i) = *(lis->olddata + i);
+    }
+
+    *(lis->data + lis->length-1) = val;
+    *(lis->data + lis->length) = '\0';
+    lis->length++;
+    free(lis->olddata);
+    
+}
+char poplist(cstring *lis) {  
+ lis->olddata = lis->data;//backup
+lis->data = (char*)malloc(sizeof(char)*(lis->length-1));
+    for (int i = 0; i < lis->length-2; i++) {
+        *(lis->data + i) = *(lis->olddata + i);
+    }
+      *(lis->data + lis->length-2) = '\0';
+
+	  char dat=*(lis->olddata+lis->length-2);
+	  
+	      lis->length--;
+	      free(lis->olddata);
+	  return dat;
+	  
+}
+void swapstr(cstring* Astr,cstring* Bstr){
+	char* Abak=Astr->data;
+	int Alen=Astr->length;
+	Astr->data=Bstr->data;
+	Astr->length=Bstr->length;
+	Bstr->data=Abak;
+	Bstr->length=Alen;
+}
+void repstr(cstring* tarstr,int where,char val){
+*(tarstr->data+where)=val;	
+}
--- cstringlib/cstring.h
+++ cstringlib/cstring.h
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define true 1
 #define false 0   
 #define bool short
-
+//原来C没有布尔值 
 typedef struct {
-	char *data;
-	char *olddata;
-	int length;
+	char *data=NULL;
+	char *olddata=NULL;
+	int length=0;
 	//bool isempty=true;//弃用！ 
 }cstring;
+
 
 int len(char* str);
 short compstr(cstring Astr,cstring Bstr);
@@ -20,3 +20,7 @@ bool setdata_lim(cstring *tar,char* st,unsigned start,unsigned int end);
 bool setdata(cstring *tar,char* st);
 bool cpstr(cstring *tostr,cstring *tarstr);
 void add_str(cstring *todata,cstring *str);
+void pushlist(cstring *lis,char val);
+char poplist(cstring *lis);
+void swapstr(cstring* Astr,cstring* Bstr);
+void repstr(cstring* tarstr,int where,char val);
